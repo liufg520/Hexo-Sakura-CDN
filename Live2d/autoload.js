64a1455e19501299f31e12638454b1e12317256a
liufg520 <945356012@qq.com>
2020-08-07 14:04:22 +0800

    最终版

--- Live2d/autoload.js
+++ Live2d/autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			apiPath: "https://cdn.jsdelivr.net/gh/liufg520/Live2D/",
+			//cdnPath: "https://cdn.jsdelivr.net/gh/liufg520/Live2D/"
 		});
 	});
 }
