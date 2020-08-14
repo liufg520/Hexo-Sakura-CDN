a116c53bdc237f0543fbded1ad2fb25d077a0c72
liufg520 <945356012@qq.com>
2020-08-14 15:15:03 +0800

    最终版

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -243,7 +243,6 @@ mashiro_global.font_control = new function () {
   }
 }()
 mashiro_global.font_control.ini()
-
 /*function code_highlight_style () {
   function gen_top_bar (i) {
     var attributes = {
@@ -281,7 +280,7 @@ mashiro_global.font_control.ini()
 }
 try {
   code_highlight_style()
-} catch (e) {}
+} catch (e) {}*/
 
 function copy_code_block () {
   $('pre code').each(function (i, block) {
@@ -289,7 +288,7 @@ function copy_code_block () {
     $(this).after('<a class="copy-code" href="javascript:" data-clipboard-target="#hljs-' + i + '" title="拷贝代码"><i class="fa fa-clipboard" aria-hidden="true"></i></a>')
   })
   var clipboard = new ClipboardJS('.copy-code')
-}*/
+}
 
 function attach_image () {
   $('#upload-img-file').change(function () {
