19d0f1868f6d54030d719bf1e50f5c93d8da48a3
liufg520 <945356012@qq.com>
2020-08-30 22:58:49 +0800

    最终版

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -243,7 +243,7 @@ mashiro_global.font_control = new function () {
   }
 }()
 mashiro_global.font_control.ini()
-/*function code_highlight_style () {
+function code_highlight_style () {
   function gen_top_bar (i) {
     var attributes = {
       'autocomplete': 'off',
@@ -280,7 +280,7 @@ mashiro_global.font_control.ini()
 }
 try {
   code_highlight_style()
-} catch (e) {}*/
+} catch (e) {}
 
 function copy_code_block () {
   $('pre code').each(function (i, block) {
@@ -289,55 +289,6 @@ function copy_code_block () {
   })
   var clipboard = new ClipboardJS('.copy-code')
 }
-
-// 代码块一键复制
-$(function () {
-  var $copyIcon = $('<i class="fa fa-copy code_copy" title="复制代码" aria-hidden="true"></i>');
-  $('.code-area').prepend($copyIcon);
-new ClipboardJS('.fa-copy', {
-  target: function (trigger) {
-      return trigger.nextElementSibling;
-  }
-});
-});
-// 代码块功能依赖
-$(function () {
-  $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
-// 代码块收缩
-$(function () {
-  var $code_expand = $('<i class="fa fa-chevron-down code-expand" title="折叠代码" aria-hidden="true"></i>');
-
-  $('.code-area').prepend($code_expand);
-  $('.code-expand').on('click', function () {
-    if ($(this).parent().hasClass('code-closed')) {
-      $(this).siblings('pre').find('code').show();
-      $(this).parent().removeClass('code-closed');
-    } else {
-      $(this).siblings('pre').find('code').hide();
-      $(this).parent().addClass('code-closed');
-    }
-  });
-});
-// 代码块语言识别
-$(function () {
-  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
-
-  $('pre').after($highlight_lang);
-  $('pre').each(function () {
-    var code_language = $(this).attr('class');
-
-if (!code_language) {
-  return true;
-};
-var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
-
-// 首字母大写
-lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-$(this).siblings(".code_lang").text(lang_name);
-
-  });
-});
 
 function attach_image () {
   $('#upload-img-file').change(function () {
